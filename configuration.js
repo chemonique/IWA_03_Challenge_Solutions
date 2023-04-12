8fe69cc2f1191ce441988e21360a6e4f56ba9665
chemonique <122254404+chemonique@users.noreply.github.com>
2023-04-12 09:47:15 +0200

    IWA3 challenge1

--- configuration.js
+++
@@ -1,3 +0,0 @@
-//IWA 3 challenge 1
-export const company = "ACME Inc.";
-export const year = 2022;
--- scripts.js
+++
@@ -1,8 +0,0 @@
-//IWA 3 Challenge 1
-
-
-import {year} from "./configuration.js";
-import {company} from "./configuration.js";
-
-const message = '© ' + company + ' (' + year + ')';
-document.querySelector('footer').innerText = message;
